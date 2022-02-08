0739cf050b8357a94d6eb065a49b5f6dcf459119
mishab <mishabz4321@gmail.com>
2022-02-08 20:26:57 +0530

    initial commi

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ y= int(input('enter value of y:'))
 
 func1= 2*x + 3*y
 func2 = 2*(x**2) + 3*y
+func3= 2*(x**3) + 3*y
 print('the value of func1 is:', func1)
 print('the value of func2 is:', func2)
+print('the value of func3 is:', func3)
