41632c8529046c5c5b65ea188c36abf692baaed1
mishab <mishabz4321@gmail.com>
2022-02-08 20:12:09 +0530

    initial python file added

---
+++ test.py
@@ -0,0 +1,7 @@
+x= int(input('enter value of x:'))
+y= int(input('enter value of y:'))
+
+func1= 2*x + 3*y
+func2 = 2*(x**2) + 3*y
+print('the value of func1 is:', func1)
+print('the value of func2 is:', func2)
